8980decda30919341c0ce161869e6c04c4a9b070
David Rajcher <david.rajcher@gmail.com>
2023-07-11 11:31:01 +0200

    ts fixed

--- server/config/db.ts
+++ server/config/db.ts
@@ -1,12 +1,12 @@
-import mongoose from "mongoose";
-import { config } from "./index";
+import mongoose from 'mongoose';
+import { config } from './index.js';
 
 export const DBconnect = async () => {
-  try {
-    mongoose.set("strictQuery", false);
-    const connection = await mongoose.connect(config.db.uri as string);
-    console.log("connection", connection.connection.host);
-  } catch (err) {
-    console.error(err);
-  }
+    try {
+        mongoose.set('strictQuery', false);
+        const connection = await mongoose.connect(config.db.uri as string);
+        console.log('connection', connection.connection.host);
+    } catch (err) {
+        console.error(err);
+    }
 };
--- server/index.ts
+++ server/index.ts
@@ -1,33 +1,21 @@
 import express, { json } from 'express';
-import { DBconnect } from './config/db';
+import { DBconnect } from './config/db.js';
 import cors from 'cors';
+
 import dotenv from 'dotenv';
-
-// Routes
-
-//PASSPORT
-import passport from 'passport';
-import session from 'express-session';
-
 dotenv.config({ path: './.env' });
+// import passport from 'passport';
 
 const PORT = process.env.PORT || 4000;
 
 const app = express();
 
 app.use(cors()).use(json());
-//    .use(
-//       session({
-//           // secret: process.env.SESSION_SECRET as string,
-//           resave: false,
-//           saveUninitialized: false,
-//       })
-//   );
 
 DBconnect();
 
-app.use(passport.initialize());
-app.use(passport.session());
+// app.use(passport.initialize());
+// app.use(passport.session());
 
 app.listen(PORT, () => {
     // eslint-disable-next-line no-undef, no-console
